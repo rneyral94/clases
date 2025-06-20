c44d33e323ce1f72caef5dcf7832385d61215377
rneyral94 <rneyral94@gmail.com>
2025-06-19 21:02:57 -0500

    Prueba de carga

---
+++ operaciones_basicas/operaciones_aritmeticas.py
@@ -0,0 +1,15 @@
+def sumar(a,b):
+    print(f"El resultado de la suma es: {a+b}")
+    return a+b
+
+def restar(a,b):
+    print(f"El resultado de la resta es: {a-b}")
+    return a-b
+
+def multiplicar(a,b):
+    print(f"El resultado de la multiplicacion es: {a*b}")
+    return a*b
+
+def dividir(a,b):
+    print(f"El resultado de la division es: {a/b}")
+    return a/b
---
+++ operaciones_basicas/operaciones_comparacion.py
@@ -0,0 +1,11 @@
+def mayor(a,b):
+    print(f"El argumento {a} es mayor que el argumento {b}")
+    return a>b
+
+def menor(a,b):
+    print(f"El argumento {a} es menor que el argumento {b}")
+    return a<b
+
+def igual(a,b):
+    print(f"El argumento {a} es igual al argumento {b}")
+    return a==b
